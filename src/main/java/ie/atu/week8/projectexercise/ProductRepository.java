2f7e01492993c8272ecba747228b9c7341075d29
Paul <paul.lennon@atu.ie>
2024-11-09 13:30:24 +0000

    Updated Repository

--- src/main/java/ie/atu/week8/projectexercise/ProductRepository.java
+++ src/main/java/ie/atu/week8/projectexercise/ProductRepository.java
@@ -1,4 +1,8 @@
 package ie.atu.week8.projectexercise;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
