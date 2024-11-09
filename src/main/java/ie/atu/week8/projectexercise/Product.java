76cfae63c31dffcaeacdcdfe71d67390bce046a8
Paul <paul.lennon@atu.ie>
2024-11-09 14:28:30 +0000

    Test change

--- src/main/java/ie/atu/week8/projectexercise/Product.java
+++ src/main/java/ie/atu/week8/projectexercise/Product.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
     //Adding a comment
+    //Adding another comment
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
