4f5bf92255f283f7afd98f22c4cd9516a7fc6b6b
Paul <paul.lennon@atu.ie>
2024-11-09 13:24:40 +0000

    Product structure and Project class

---
+++ src/main/java/ie/atu/week8/projectexercise/ProjectExerciseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week8.projectexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week8/projectexercise/ProjectExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week8.projectexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
